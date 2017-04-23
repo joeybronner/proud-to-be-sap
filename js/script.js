b987dea4d8dfdaeb3adc9b9c58bc45281777251d
Joey Bronner <joeybronner@gmail.com>
2017-04-23 21:34:20 +0200

    Favicon and script to a JS file

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var canvas = document.getElementById('c');
+var video = document.getElementById('v');
+var img1 = document.getElementById('mask');
+
+navigator.getUserMedia({video: true}, function(stream) {
+    video.srcObject = stream;
+    video.play();
+}, function(err) { alert("there was an error " + err)});
+
+function download() {
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(video, 0, 0, 640, 480);
+    ctx.drawImage(img1, 0, 0, 640, 480);
+
+    var dt = canvas.toDataURL('image/jpeg');
+    this.href = dt;
+};
+downloadLnk.addEventListener('click', download, false);
