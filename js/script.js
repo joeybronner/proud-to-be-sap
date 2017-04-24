a0dd3504ae8729faddc16959d450e21a5eda02c1
Joey Bronner <joeybronner@gmail.com>
2017-04-24 20:18:45 +0200

    Default image and ruban with error message if webcam is not allowed

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-var canvas = document.getElementById('c');
-var video = document.getElementById('v');
+var canvas = document.getElementById('canvas');
+var video = document.getElementById('video');
 var buttonnext = document.getElementById('btnext');
 var buttonprevious = document.getElementById('btprevious');
-var img1 = document.getElementById('mask');
+var mask = document.getElementById('mask');
+var defaultPeople = document.getElementById('default-people');
 var msg = document.getElementById('msg');
 var aMasks = ['mask1.png', 'mask2.png'];
 
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
 function onSuccess (mediaObj){
-    img1.style.visibility = 'visible';
     msg.style.visibility = 'hidden';
+    defaultPeople.style.visibility = 'hidden';
     window.stream = mediaObj;
     var video = document.querySelector('video');
     video.src = window.URL.createObjectURL(mediaObj);
@@ -26,7 +27,7 @@ navigator.getUserMedia({ video: true }, onSuccess, onError);
 function download () {
     var ctx = canvas.getContext('2d');
     ctx.drawImage(video, 0, 0, 640, 480);
-    ctx.drawImage(img1, 0, 0, 640, 480);
+    ctx.drawImage(mask, 0, 0, 640, 480);
 
     var dt = canvas.toDataURL('image/jpeg');
     this.href = dt;
@@ -35,27 +36,27 @@ function download () {
 downloadLnk.addEventListener('click', download, false);
 
 function next () {
-    var sCurrentMaskTemp = img1.src;
+    var sCurrentMaskTemp = mask.src;
     var sCurrentMask = sCurrentMaskTemp.substring(sCurrentMaskTemp.lastIndexOf('/') + 1, sCurrentMaskTemp.length);
     var iCurrentMask = aMasks.indexOf(sCurrentMask);
     var iExpectedMask = iCurrentMask + 1;
     var iMasksLength = aMasks.length;
 
     if (iExpectedMask < iMasksLength) {
-        img1.src = 'img/' + aMasks[iExpectedMask];
+        mask.src = 'img/' + aMasks[iExpectedMask];
     }
 
     buttonAvailabilities(iExpectedMask);
 }
 
 function previous() {
-    var sCurrentMaskTemp = img1.src;
+    var sCurrentMaskTemp = mask.src;
     var sCurrentMask = sCurrentMaskTemp.substring(sCurrentMaskTemp.lastIndexOf('/') + 1, sCurrentMaskTemp.length);
     var iCurrentMask = aMasks.indexOf(sCurrentMask);
     var iExpectedMask = iCurrentMask - 1;
 
     if (iExpectedMask >= 0) {
-        img1.src = 'img/' + aMasks[iExpectedMask];
+        mask.src = 'img/' + aMasks[iExpectedMask];
     }
 
     buttonAvailabilities(iExpectedMask);
